4dc1d58bdcb04e18c68fa4557a00bc4619f69c68
yehiahassanain <89047698+yehiahassanain@users.noreply.github.com>
2026-03-12 16:01:52 +0200

    home page

--- GridNox.AI/src/components/Home/Home.jsx
+++ GridNox.AI/src/components/Home/Home.jsx
@@ -1,27 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import Style from './Home.module.css'
-import video from '../../assets/images/video.mp4'
+import React, { useEffect, useState } from "react";
+import Style from "./Home.module.css";
+import video from "../../assets/images/video.mp4";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
-    return (
-        <section  >
-            <video 
-                className={Style.backgroundVideo} 
-                autoPlay 
-                loop 
-                muted 
-                playsInline
+  return (
+    <>
+      <div className="container">
+        <div >
+          <section className="container">
+            <video
+              className={Style.backgroundVideo}
+              autoPlay
+              loop
+              muted
+              playsInline
             >
-                <source src={video} type="video/mp4" />
+              <source src={video} type="video/mp4" />
             </video>
             <div className={Style.overlay}></div>
-            <div className={Style.contentContainer}>
-                <h1 className={Style.mainHeading}>Securing the Digital Future</h1>
-                <p className={Style.subHeading}>Advanced Threat Intelligence & Cybersecurity Solutions</p>
-                <button className={Style.ctaButton}>Discover Our Solutions</button>
+            <div className={`${Style.contentContainer} mt-25 p-15`}>
+              <h1 className="text-white md:text-4xl sm:text-3xl font-bold mb-5">
+                At GridNox.AI, we excel in
+              </h1>
+              <p
+                className={`text-red-500 font-semibold md:text-3xl sm:text-2xl mb-5 ${Style.slideInLeft}`}
+              >
+                GRC automation & Cybersecurity consulting
+              </p>
+              <p className="text-white text-semibold md:text-xl sm:text-l mb-5">
+                GridNox.ai delivers GRC automation, Cybersecurity consulting,
+                and professional services to streamline compliance, optimize
+                operations, and accelerate growth. We combine practical
+                expertise and scalable solutions to drive measurable efficiency
+                and risk reduction for large and small/medium Enterprises.
+              </p>
+              <NavLink to="/services">
+                <button className={Style.ctaButton}>
+                  Discover Our Solutions
+                </button>
+              </NavLink>
             </div>
-        </section>
-    );
+          </section>
+        </div>
+        <div className="bg-[#101010] p-15 w-full absolute ">
+            <h2 className="text-[#bc2710] text-[56px] md:text-[80px] xl:text-[96px] pb-[20px]">we help</h2>
+            <p className ="text-white text-[32px] xl:text-[55px]">In Creating Strategies, Design and Development.</p>
+        </div>
+      </div>
+    </>
+  );
 }
-
-
--- GridNox.AI/src/components/Navbar/Navbar.jsx
+++ GridNox.AI/src/components/Navbar/Navbar.jsx
@@ -1,70 +1,101 @@
 import React, { useEffect, useState } from "react";
-import Style from "./Navbar.module.css";
 import logo from "../../assets/images/gridnox_ai_logo1.png";
+import logo1 from "../../assets/images/gridnox_ai_logo.png";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+
   const [isScrolled, setIsScrolled] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 20);
     };
+
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
+
   }, []);
 
   return (
-    <>
-      <nav className={`fixed top-0 left-0 w-full z-150 h-50px md:h-16 lg:h-76px flex items-center justify-between px-6 pt-4 pb-18px md:py-4 xl:px-120px transition-all duration-300 ${isScrolled ? 'bg-white  shadow-lg' : 'bg-transparent drop-shadow-lg'}`} >
-        <div className="container flex justify-around items-center mx-auto py-4">
-          <NavLink to="/">
-            <img
-              src={logo}
-              className="h-40"
-              alt="gridnox Logo"
-            />
-          </NavLink>
-          <div
-            className="items-center justify-between w-full md:flex md:w-auto md:order-1"
-          >
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
-              <li>
-                <NavLink
-                  to='/'
-                  className="block font-bold py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0"
-                  aria-current="page"
-                >
-                  Home
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/about'
-                  className="block font-bold py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-                >
-                  About
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/services'
-                  className="block font-bold py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-                >
-                  Services
-                </NavLink>
-              </li>
-              <li>
-                <NavLink
-                  to='/contact-us'
-                  className="block font-bold py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-                >
-                  Contact
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
+    <nav
+      className={`fixed top-0 left-0 w-full z-50 transition-all duration-300
+      ${isScrolled ? "bg-white shadow-lg" : "bg-transparent"}`}
+    >
+      <div className="max-w-7xl h-20 mx-auto flex items-center justify-between px-6 py-4">
+
+        {/* Logo */}
+        <NavLink to="/">
+          <img
+            src={isScrolled ? logo : logo1}
+            className="h-30 md:h-44"
+            alt="gridnox logo"
+          />
+        </NavLink>
+
+        {/* Hamburger Button (Mobile) */}
+        <button
+          className={`md:hidden text-3xl ${
+            isScrolled ? "text-black" : "text-white"
+          }`}
+          onClick={() => setMenuOpen(!menuOpen)}
+        >
+          ☰
+        </button>
+
+        {/* Desktop Menu */}
+        <ul
+          className={`font-medium md:flex md:space-x-8 absolute md:static left-0 w-full md:w-auto
+          transition-all duration-300
+          ${menuOpen ? "top-16 bg-white shadow-md" : "top-[-400px]"}
+          md:bg-transparent md:shadow-none text-center `}
+        >
+          <li>
+            <NavLink
+              to="/"
+              className={`block px-6 py-3 sm:text-black md:p-0 font-bold md:${
+                isScrolled ? "text-black" : "text-white"
+              }`}
+            >
+              Home
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/about"
+              className={`block px-6 py-3 sm:text-black md:p-0 font-bold md:${
+                isScrolled ? "text-black" : "text-white"
+              }`}
+            >
+              About
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/services"
+              className={`block px-6 py-3 sm:text-black md:p-0 font-bold md:${
+                isScrolled ? "text-black" : "text-white"
+              }`}
+            >
+              Services
+            </NavLink>
+          </li>
+
+          <li>
+            <NavLink
+              to="/contact-us"
+              className={`block px-6 py-3 sm:text-black md:p-0 font-bold md:${
+                isScrolled ? "text-black" : "text-white"
+              }`}
+            >
+              Contact
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
